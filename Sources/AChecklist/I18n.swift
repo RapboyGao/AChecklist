724ce235cd0fc9a49ee880f06a35e02b7f3afd87
RapboyGao <atgao74201@qq.com>
2025-11-04 10:23:10 +0800

    feat(AChecklistItemEditContent): 添加过期时间设置功能
    为 AChecklistItemEditContent 视图添加了设置过期时间的功能，包括秒、分钟、小时、天、月和年的输入字段
    同时更新了 I18n 文件以支持多语言显示时间单位

--- Sources/AChecklist/I18n.swift
+++ Sources/AChecklist/I18n.swift
@@ -1,12 +1,76 @@
 import Foundation
 
-public enum I18n {
+enum I18n {
   public static let mutualExclusion = NSLocalizedString(
     "@mutualExclusion",
     bundle: .module, comment: "AChecklistSectionEditView 中 MutualExclusion 开关的标签文字")
+
+  public static let checklistItem = NSLocalizedString(
+    "@checklistItem",
+    bundle: .module, comment: "AChecklistItemEditView 的标题")
 
   /// 启用 MutualExclusion 后，同一 Checklist 内相邻 Section 将互斥，仅允许选择其中一组
   public static let mutualExclusionExplanation = NSLocalizedString(
     "@mutualExclusionExplanation",
     bundle: .module, comment: "AChecklistSectionEditView 中解释什么是 MutualExclusion 的描述性文字")
+
+  public static let second = NSLocalizedString(
+    "@second",
+    bundle: .module, comment: "AChecklistItemEditView 中设置秒数的标签文字(单数形式)")
+
+  public static let seconds = NSLocalizedString(
+    "@seconds",
+    bundle: .module, comment: "AChecklistItemEditView 中设置秒数的标签文字(复数形式)")
+
+  public static let minute = NSLocalizedString(
+    "@minute",
+    bundle: .module, comment: "AChecklistItemEditView 中设置分钟数的标签文字(单数形式)")
+
+  public static let expiresAfter = NSLocalizedString(
+    "@expiresAfter",
+    bundle: .module, comment: "AChecklistItemEditView 中设置过期时间的Section标题")
+
+  public static let minutes = NSLocalizedString(
+    "@minutes",
+    bundle: .module, comment: "AChecklistItemEditView 中设置分钟数的标签文字(复数形式)")
+
+  public static let hour = NSLocalizedString(
+    "@hour",
+    bundle: .module, comment: "AChecklistItemEditView 中设置小时数的标签文字(单数形式)")
+
+  public static let hours = NSLocalizedString(
+    "@hours",
+    bundle: .module, comment: "AChecklistItemEditView 中设置小时数的标签文字(复数形式)")
+
+  public static let day = NSLocalizedString(
+    "@day",
+    bundle: .module, comment: "AChecklistItemEditView 中设置天数的标签文字(单数形式)")
+
+  public static let days = NSLocalizedString(
+    "@days",
+    bundle: .module, comment: "AChecklistItemEditView 中设置天数的标签文字(复数形式)")
+
+  public static let week = NSLocalizedString(
+    "@week",
+    bundle: .module, comment: "AChecklistItemEditView 中设置周数的标签文字(单数形式)")
+
+  public static let weeks = NSLocalizedString(
+    "@weeks",
+    bundle: .module, comment: "AChecklistItemEditView 中设置周数的标签文字(复数形式)")
+
+  public static let month = NSLocalizedString(
+    "@month",
+    bundle: .module, comment: "AChecklistItemEditView 中设置月数的标签文字(单数形式)")
+
+  public static let months = NSLocalizedString(
+    "@months",
+    bundle: .module, comment: "AChecklistItemEditView 中设置月数的标签文字(复数形式)")
+
+  public static let year = NSLocalizedString(
+    "@year",
+    bundle: .module, comment: "AChecklistItemEditView 中设置年数的标签文字(单数形式)")
+
+  public static let years = NSLocalizedString(
+    "@years",
+    bundle: .module, comment: "AChecklistItemEditView 中设置年数的标签文字(复数形式)")
 }
--- Sources/AChecklist/Views/EditViews/Item/AChecklistItemEditContent.swift
+++ Sources/AChecklist/Views/EditViews/Item/AChecklistItemEditContent.swift
@@ -3,38 +3,182 @@ import SwiftUI
 
 #if !os(watchOS)
 
-  @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 10.0, *)
-  public struct AChecklistItemEditContent: View {
-    @Binding var item: AChecklistItem
+    @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 10.0, *)
+    public struct AChecklistItemEditContent: View {
+        @Binding var item: AChecklistItem
 
-    public var body: some View {
-      List {
-        HStack {
-          Image(
-            systemName: SwiftI18n.name
-              .defaultSystemImage
-          )
-          .foregroundColor(.accentColor)
-          TextField(
-            SwiftI18n.title.description,
-            text: $item.title
-          )
-          .multilineTextAlignment(.trailing)
+        @ViewBuilder
+        private var secondsHStack: some View {
+            HStack {
+                TextField(
+                    I18n.seconds,
+                    value: $item.expiresAfter.second,
+                    format: .number
+                )
+                #if !os(macOS)
+                    .keyboardType(.numberPad)
+                #endif
+                switch item.expiresAfter.second {
+                case 0:
+                    Text(I18n.second)
+                default:
+                    Text(I18n.seconds)
+                }
+            }
         }
-        VStack(alignment: .leading) {
-          SwiftI18n.descriptionKey.text
-            .padding(.vertical)
-          TextEditor(text: $item.detail)
-            .frame(minHeight: 100)
-        }
-      }
-      .navigationTitle(item.title)
 
+        @ViewBuilder
+        private var minutesHStack: some View {
+            HStack {
+                TextField(
+                    I18n.minutes,
+                    value: $item.expiresAfter.minute,
+                    format: .number
+                )
+                #if !os(macOS)
+                    .keyboardType(.numberPad)
+                #endif
+                switch item.expiresAfter.minute {
+                case 0:
+                    Text(I18n.minute)
+                default:
+                    Text(I18n.minutes)
+                }
+            }
+        }
+
+        @ViewBuilder
+        private var hoursHStack: some View {
+            HStack {
+                TextField(
+                    I18n.hours,
+                    value: $item.expiresAfter.hour,
+                    format: .number
+                )
+                #if !os(macOS)
+                    .keyboardType(.numberPad)
+                #endif
+                switch item.expiresAfter.hour {
+                case 0:
+                    Text(I18n.hour)
+                default:
+                    Text(I18n.hours)
+                }
+            }
+        }
+
+        @ViewBuilder
+        private var daysHStack: some View {
+            HStack {
+                TextField(
+                    I18n.days,
+                    value: $item.expiresAfter.day,
+                    format: .number
+                )
+                #if !os(macOS)
+                    .keyboardType(.numberPad)
+                #endif
+                switch item.expiresAfter.day {
+                case 0:
+                    Text(I18n.day)
+                default:
+                    Text(I18n.days)
+                }
+            }
+        }
+
+        @ViewBuilder
+        private var monthHStack: some View {
+            HStack {
+                TextField(
+                    I18n.months,
+                    value: $item.expiresAfter.month,
+                    format: .number
+                )
+                #if !os(macOS)
+                    .keyboardType(.numberPad)
+                #endif
+                switch item.expiresAfter.month {
+                case 0:
+                    Text(I18n.month)
+                default:
+                    Text(I18n.months)
+                }
+            }
+        }
+
+        @ViewBuilder
+        private var yearHStack: some View {
+            HStack {
+                TextField(
+                    I18n.years,
+                    value: $item.expiresAfter.year,
+                    format: .number
+                )
+                #if !os(macOS)
+                    .keyboardType(.numberPad)
+                #endif
+                switch item.expiresAfter.year {
+                case 0:
+                    Text(I18n.year)
+                default:
+                    Text(I18n.years)
+                }
+            }
+        }
+
+        public var body: some View {
+            List {
+                Section(I18n.checklistItem) {
+                    TextField(
+                        SwiftI18n.title.description,
+                        text: $item.title
+                    )
+                    .font(.title)
+                    .padding(.vertical)
+                    TextEditor(text: $item.detail)
+                        .frame(minHeight: 100)
+                }
+
+                Section(I18n.expiresAfter) {
+                    secondsHStack
+                    minutesHStack
+                    hoursHStack
+                    daysHStack
+                    monthHStack
+                    yearHStack
+                }
+
+            }
+            .navigationTitle(item.title)
+
+        }
+
+        public init(_ item: Binding<AChecklistItem>) {
+            self._item = item
+        }
     }
 
-    public init(_ item: Binding<AChecklistItem>) {
-      self._item = item
-    }
-  }
+    @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 10.0, *)
+    private struct Example: View {
+        @State var item: AChecklistItem = AChecklistItem(
+            title: "Example",
+            detail: "Example Detail"
+        )
 
+        var body: some View {
+            AChecklistItemEditContent($item)
+        }
+    }
+
+    @available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+    struct AChecklistItemEditContent_Previews: PreviewProvider {
+        static var previews: some View {
+            NavigationStack {
+                Example()
+
+            }
+
+        }
+    }
 #endif
